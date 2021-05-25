734d2ac18b31cfc07914289c27fb91804f0c6bcb
unknown <tyashaswiad@gmail.com>
2021-05-25 13:08:14 +0530

    changed provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
   version = "~> 3.0"
-  access_key = "AKIAUYAC7DWPJAYY2E53"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
